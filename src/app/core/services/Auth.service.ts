6f817f17b201ffb0f6a816a05af7cb230feb6afb
MinaAl5al <minanasser82018@gmail.com>
2025-05-06 22:27:31 +0300

    getUserRole

--- src/app/core/services/Auth.service.ts
+++ src/app/core/services/Auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
   isLoggedUser(): boolean {
     return (localStorage.getItem('Access_Token') == null) ? false : true
   }
-  getToken():string{
+  getToken(): string {
     return localStorage.getItem('Access_Token') ?? ""
   }
 
@@ -25,5 +25,18 @@ export class AuthService {
     localStorage.removeItem('Access_Token')
     this.isLoggedUserSubject.next(false)
   }
+  getUserRole(): string {
+    const token = this.getToken();
+    if (!token) return '';
+
+    try {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      return payload.role || '';
+    } catch (error) {
+      console.error('Invalid token format', error);
+      return '';
+    }
+  }
+
 
 }
