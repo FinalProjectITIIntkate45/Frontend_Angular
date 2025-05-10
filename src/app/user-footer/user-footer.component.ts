202ba6a5a3ed9ec7a3dbaa10a60ff7448d831562
sama <sama.sama152000@gmail.com>
2025-05-10 18:09:59 +0300

    ..

--- src/app/user-footer/user-footer.component.ts
+++ src/app/user-footer/user-footer.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-user-footer',
   templateUrl: './user-footer.component.html',
   styleUrls: ['./user-footer.component.css']
-  styleUrls: ['./user-footer.component.css']
 })
 export class UserFooterComponent {
 }
