6bdc427981975ee6ae222e47dc528602f9d86d54
MinaAl5al <minanasser82018@gmail.com>
2025-05-26 21:05:13 +0300

    Auth

--- src/app/core/interceptors/AuthInterceptor.ts
+++ src/app/core/interceptors/AuthInterceptor.ts
@@ -6,11 +6,13 @@ export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
 
+  console.log(token);
   if (token) {
     const authReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`),
+
     });
-   
+   console.log(authReq);
     return next(authReq);
   }
 
