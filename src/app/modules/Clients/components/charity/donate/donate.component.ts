069c66d901daec919f63a0e2962cee6536891fb3
Nada-Elamin <dev.nadaelamin@gmail.com>
2025-06-18 17:26:44 +0300

    solve shop_sectionCon

--- src/app/modules/Clients/components/charity/donate/donate.component.ts
+++ src/app/modules/Clients/components/charity/donate/donate.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CharityService } from '../../../Services/Charity.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-donate',
-  
   templateUrl: './donate.component.html',
-  styleUrl: './donate.component.css',
-  standalone:false,
+  styleUrls: ['./donate.component.css'],
+  standalone:false
 })
-export class DonateComponent {
+export class DonateComponent implements OnInit {
+  donateForm!: FormGroup;
+  charityId!: number;
 
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private charityService: CharityService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.charityId = Number(this.route.snapshot.paramMap.get('id'));
+    this.donateForm = this.fb.group({
+      amount: [null, [Validators.required, Validators.min(1)]]
+    });
+  }
+
+  submitDonation(): void {
+    if (this.donateForm.invalid) return;
+
+    const donationData = {
+      charityId: this.charityId,
+      amount: this.donateForm.value.amount
+    };
+
+    this.charityService.donateToCharity(donationData).subscribe({
+      next: (res) => {
+        alert('Donation successful!');
+        this.router.navigate(['/clients/charities']);
+      },
+      error: (err) => {
+        console.error('Donation error:', err);
+        alert('Donation failed. Try again.');
+      }
+    });
+  }
 }
