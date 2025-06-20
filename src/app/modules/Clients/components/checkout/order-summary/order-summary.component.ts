82d42a6f066d271109374e007697b14f34576ad7
Nada-Elamin <dev.nadaelamin@gmail.com>
2025-06-20 19:41:03 +0300

    orderConf

--- src/app/modules/Clients/components/checkout/order-summary/order-summary.component.ts
+++ src/app/modules/Clients/components/checkout/order-summary/order-summary.component.ts
@@ -7,17 +7,17 @@ import { CartServicesService } from '../../../Services/CardServices.service';
 @Component({
   selector: 'app-order-summary',
   standalone: false,
-  templateUrl: './order-summary.component.html',
+  templateUrl:'./order-summary.component.html',
   styleUrls: ['./order-summary.component.css']
 })
 export class OrderSummaryComponent implements OnInit {
   cartData: CartInterface | null = null;
   isLoading: boolean = true;
 
-  constructor(private cartService: CartServicesService) {}
+  constructor(private cartService?: CartServicesService) {}
 
   ngOnInit(): void {
-    this.cartService.getCartItems().subscribe({
+    this.cartService?.getCartItems().subscribe({
       next: (data) => {
         this.cartData = data;
         this.isLoading = false;
