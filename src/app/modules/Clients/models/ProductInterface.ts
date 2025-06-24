44d7f24342e129ddef3f783f221d06a55df045db
makaiouseliaa@gmail.commakarioselia <makaiouseliaa@gmail.com>
2025-06-25 02:08:29 +0300

    solve conflect

--- src/app/modules/Clients/models/ProductInterface.ts
+++ src/app/modules/Clients/models/ProductInterface.ts
@@ -1,18 +1,8 @@
 export interface ProductInterface {
-    Id: number;
-    Name: string;
-    Description: string;
-    Stock: number;
-    BasePrice: number;
-    Points: number;
-    CategoryName: string;
-    ShopName: string;
-    CreatedAt: string;
-    Images: string[];
-    ModificationDate: string;
-    CategoryId: number;
-    DisplayedPrice: number;
-    DisplayedPriceAfterDiscount?: number;
-    IsSpecialOffer: boolean;
-    EarnedPoints: number;
+    id: number;
+    name: string;
+    description: string;
+    displayedPrice: number;
+    displayedPriceAfterDiscount: number;
+    imageUrl: string;
 }
