7b45ff1ad7682c15d5fb029c94c6fe03c37ab4fb
MinaAl5al <minanasser82018@gmail.com>
2025-07-02 04:48:32 +0300

    Add Wallet Point

--- src/app/modules/Clients/Services/wallet.service.ts
+++ src/app/modules/Clients/Services/wallet.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { environment } from '../../../../environments/environment';
+
 import { ShopPoint, FreePoint, WalletView } from '../Models/wallet.models';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
