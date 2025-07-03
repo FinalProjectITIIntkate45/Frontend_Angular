10572cf577c8eca344c1f04eb0c57eb647efd85b
MinaAl5al <minanasser82018@gmail.com>
2025-07-03 14:08:47 +0300

    Edit Wallet

--- src/app/modules/Clients/Services/wallet.service.ts
+++ src/app/modules/Clients/Services/wallet.service.ts
@@ -11,30 +11,59 @@ import { environment } from '../../../../environments/environment';
   providedIn: 'root',
 })
 export class WalletService {
-  private apiUrl = environment.apiUrl + '/ShopPoint';
+  private apiUrl = environment.apiUrl + '/Wallet';
 
   constructor(private http: HttpClient) {}
 
   getWalletView(userId: string): Observable<WalletView> {
     return this.http
-      .get<WalletView>(`${this.apiUrl}/user/${userId}/wallet`)
+      .get<WalletView>(`${this.apiUrl}/${userId}`)
       .pipe(catchError(this.handleError));
   }
 
   getShopPoints(userId: string): Observable<ShopPoint[]> {
     return this.http
-      .get<ShopPoint[]>(`${this.apiUrl}/user/${userId}`)
+      .get<ShopPoint[]>(`${this.apiUrl}/${userId}/shop-points`)
       .pipe(catchError(this.handleError));
   }
 
-  getFreePoints(userId: string): Observable<FreePoint[]> {
+  getFreePoints(): Observable<FreePoint[]> {
     return this.http
-      .get<FreePoint[]>(`${this.apiUrl}/user/${userId}/free`)
+      .get<FreePoint[]>(`${this.apiUrl}/GetFreePoints`)
+      .pipe(catchError(this.handleError));
+  }
+
+  getPendingPoints(userId: string): Observable<FreePoint[]> {
+    return this.http
+      .get<FreePoint[]>(`${this.apiUrl}/${userId}/pending-points`)
+      .pipe(catchError(this.handleError));
+  }
+
+  createWallet(userId: string): Observable<WalletView> {
+    return this.http
+      .post<WalletView>(`${this.apiUrl}`, { userId })
+      .pipe(catchError(this.handleError));
+  }
+
+  updatePoints(userId: string, amount: number): Observable<WalletView> {
+    return this.http
+      .put<WalletView>(`${this.apiUrl}/${userId}/points`, amount)
+      .pipe(catchError(this.handleError));
+  }
+
+  updateCashBalance(amount: number): Observable<string> {
+    return this.http
+      .put<string>(`${this.apiUrl}/UpdateWalletCashBalance`, amount)
+      .pipe(catchError(this.handleError));
+  }
+
+  deleteWallet(): Observable<string> {
+    return this.http
+      .delete<string>(`${this.apiUrl}/DeleteWallet`)
       .pipe(catchError(this.handleError));
   }
 
   private handleError(error: any) {
-    // You can customize error handling here
     return throwError(() => error);
   }
 }
