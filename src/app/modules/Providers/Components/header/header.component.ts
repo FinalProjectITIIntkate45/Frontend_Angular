499175214b6202d284657a8c49a1ab0eabf9c730
Sama <sama.sama152000@gmail.com>
2025-07-06 21:32:14 +0300

    Vendor profile edits

--- src/app/modules/Providers/Components/header/header.component.ts
+++ src/app/modules/Providers/Components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';  
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HeaderComponent {
-isDropdownOpen: any;
-  constructor() { }
+  isDropdownOpen = false;
+
+  constructor(private router: Router) {}
+
+  logout(): void {
+  // مسح البيانات المحددة فقط من localStorage
+  localStorage.removeItem('token');
+  localStorage.removeItem('providerId');
+  localStorage.removeItem('userId');
+
+  // لو فيه أي حاجة كنتي بتحطيها في sessionStorage
+  sessionStorage.removeItem('token');
+  sessionStorage.removeItem('providerId');
+  sessionStorage.removeItem('userId');
+
+  this.router.navigate(['/']); 
+}
+
 }
