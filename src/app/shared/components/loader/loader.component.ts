6f2294b785f1f76c26120f760d367636f9665a73
MinaAl5al <minanasser82018@gmail.com>
2025-07-15 08:52:54 +0300

    Loader

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.css'],
+  imports: [FormsModule, CommonModule],
 })
 export class LoaderComponent {
   @Input() isLoading: boolean = false;
